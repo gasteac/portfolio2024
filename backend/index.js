bda414226d2c482ad703c88ee629dcadbb19f32a
Gaston Acosta <gasteac@gmail.com>
2024-05-07 20:47:52 -0300

    chore: Update server configuration to serve static files from frontend/dist directory

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
 require("dotenv").config();
 const cors = require("cors");
 const { dbConnection } = require("./database/config");
@@ -7,7 +9,10 @@ dbConnection();
 app.use(cors());
 app.use(express.json());
 app.use("/api/form", require("./routes/message"));
-app.use('*', (req, res)=>{res.sendFile(__dirname + '/index.html')});
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 app.listen(process.env.PORT, () => {
   console.log(`Running at port ${process.env.PORT}`);
 });
