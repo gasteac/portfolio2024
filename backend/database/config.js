e828af0f9bf742d9c66c43dca4969b7a772ae5b0
Gaston Acosta <gasteac@gmail.com>
2024-05-07 20:40:13 -0300

    refactor database connection configuration

--- backend/database/config.js
+++ backend/database/config.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.DB_CNN, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
+    await mongoose.connect(process.env.DB_CNN);
     console.log('DB Online');
   } catch (error) {
+    console.log(process.env.DB_CNN)
     console.log(error);
         throw new Error('Error initializing BD'); 
   }
--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ dbConnection();
 app.use(cors());
 app.use(express.json());
 app.use("/api/form", require("./routes/message"));
-// app.use('*', (req, res)=>{res.sendFile(__dirname + '/index.html')});
+app.use('*', (req, res)=>{res.sendFile(__dirname + '/index.html')});
 app.listen(process.env.PORT, () => {
   console.log(`Running at port ${process.env.PORT}`);
 });
