10537ce80b813db8411479656d08134125e435b7
Gaston Acosta <gasteac@gmail.com>
2024-05-16 01:21:42 -0300

    minor changes in home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -15,12 +15,9 @@ export const Home = ({ handleClickScroll, setActiveLink }) => {
   return (
     <>
       <div className="relative h-[100vh] justify-center  flex w-screen  items-center  tablet:h-[100vh] overflow-hidden">
-        {showRobot ? <Fade className='absolute w-full -right-2 h-full z-0 overflow-clip' triggerOnce={true}  duration={2000}>
+        {showRobot && <Fade className='absolute w-full -right-2 h-full z-0 overflow-clip' triggerOnce={true}  duration={2000}>
           <Spline className="overflow-clip" scene="https://prod.spline.design/1Np5iFnYpUkyE9pK/scene.splinecode" />
-        </Fade> :
-           <div className='absolute laptop:right-[162px] z-0 opacity-80'>
-          <Spline className="scale-75 phone:scale-105 transition-all " scene="https://prod.spline.design/aZZ5kX7gJc2MkRAO/scene.splinecode" />
-      </div>}
+        </Fade>}
         <div>
           <Presentation handleShowRobot={handleShowRobot} showRobot={showRobot}/>
         </div>
